037a433dbd00aa9245abb10b59086721bc9d04d3
EliSchaar <elidschaar@gmail.com>
2026-02-21 12:23:03 -0600

    added functions to send data to google sheets, find the credentials file in discord

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -14,11 +17,45 @@ app.get('/', (req, res) => {
 });
 
 // Mileage POST route
-app.post('/submit-mileage', (req, res) => {
-    console.log("Received data:", req.body);
-    res.json({ message: "Mileage received!" });
-});
+app.post('/submit-mileage', async (req, res) => {
+    try {
+        console.log("Received:", req.body);
 
+        await appendToSheet(req.body);
+
+        res.json({ message: "Mileage saved to Google Sheets!" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error saving mileage" });
+    }
+});
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+async function appendToSheet(data) {
+    const client = await auth.getClient();
+    const sheets = google.sheets({ version: 'v4', auth: client });
+
+    const spreadsheetId = '1Qtf6MsIzHvdugmba-Ti_xqL5iYVBh3ChZIj3QPoSAng';
+
+    await sheets.spreadsheets.values.append({
+        spreadsheetId,
+        range: 'Sheet1!A:D',
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+            values: [[
+                data.name,
+                data.date,
+                data.miles,
+                data.notes
+            ]]
+        }
+    });
+}
